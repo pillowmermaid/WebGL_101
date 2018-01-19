4a2da8584a0405dbc9b311cefd0ffbef1e480372
Amir Ashtiani <pillowmermaid@gmail.com>
2018-01-18 23:26:36 -0500

    starting to add rotation

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var positionLocation;
 var resolutionLocation;
 var colorLocation;
 var translationLocation;
+var rotationLocation;
 
 var positionBuffer;
 
@@ -18,8 +19,9 @@ var mouseX = 0;
 var mouseY = 0;
 var frame = 0;
 
-var rect = {
+var shape = {
   translation: [0, 0],
+  rotation: [0, -1],
   width: 100,
   height: 30,
   color: [Math.random(), Math.random(), Math.random(), 1]
@@ -32,7 +34,7 @@ function init() {
   canvas.height = 300;
 
   gl = canvas.getContext("webgl");
-  if(!gl){
+  if (!gl) {
     //i guess you aint got it
   }
 
@@ -45,6 +47,7 @@ function init() {
   resolutionLocation = gl.getUniformLocation(program, "u_resolution");
   colorLocation = gl.getUniformLocation(program, "u_color");
   translationLocation = gl.getUniformLocation(program, "u_translation");
+  rotationLocation = gl.getUniformLocation(program, "u_rotation");
 
   positionBuffer = gl.createBuffer();
 
@@ -58,8 +61,8 @@ function init() {
 }
 
 function updatePosition(evt) {
-  	mouseX = evt.clientX * 2;
-  	mouseY = evt.clientY * 2;
+  mouseX = evt.clientX;
+  mouseY = evt.clientY;
 }
 
 function createRectangle(gl, x, y, w, h) {
@@ -70,6 +73,7 @@ function createRectangle(gl, x, y, w, h) {
 
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]), gl.STATIC_DRAW);
 }
+
 function createLetterF(gl) {
   gl.bufferData(
     gl.ARRAY_BUFFER,
@@ -96,7 +100,8 @@ function createLetterF(gl) {
       30, 90,
       30, 90,
       67, 60,
-      67, 90]), gl.STATIC_DRAW);
+      67, 90
+    ]), gl.STATIC_DRAW);
 }
 
 
@@ -107,9 +112,10 @@ function createLetterF(gl) {
 function update() {
   frame++;
   // if(frame % 20 === 0){
-  //   rect.color = [Math.random(), Math.random(), Math.random(), 1];
+  //   shape.color = [Math.random(), Math.random(), Math.random(), 1];
   // }
-  rect.translation = [mouseX, mouseY];
+  shape.translation = [mouseX, mouseY];
+
 
   render();
 }
@@ -125,10 +131,10 @@ function render() {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   //Sets an iterator to go through the buffer data
-  var size = 2;  //how many values to return per iteration
+  var size = 2; //how many values to return per iteration
   var type = gl.FLOAT; //what type they are
   var normalize = false;
-  var stride = 0;  //how many spots to jump per iteration
+  var stride = 0; //how many spots to jump per iteration
   var offset = 0; //how many spots to offset the start by
 
   // passes an array of 2d vectors to the positionAttribute}
@@ -137,9 +143,10 @@ function render() {
   // sets the 2d vector that resolutionLocation points at to {canvasW, canvasH}
   gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
 
-  gl.uniform4fv(colorLocation, rect.color);
+  gl.uniform4fv(colorLocation, shape.color);
 
-  gl.uniform2fv(translationLocation, rect.translation);
+  gl.uniform2fv(translationLocation, shape.translation);
+  gl.uniform2fv(rotationLocation, shape.rotation);
 
   var primitiveType = gl.TRIANGLES;
   var offset = 0;
@@ -150,8 +157,8 @@ function render() {
   //~~~OLD BUSTED~~~
   //~~~~~~~~~~~~~~~~
   // for (var i = 0; i < 1; i++){
-  //   createRectangle(gl, rect.translation[0], rect.translation[1], rect.width, rect.height);
-  //   gl.uniform4f(colorLocation, rect.color[0], rect.color[1], rect.color[2], rect.color[3]);
+  //   createRectangle(gl, shape.translation[0], shape.translation[1], shape.width, shape.height);
+  //   gl.uniform4f(colorLocation, shape.color[0], shape.color[1], shape.color[2], shape.color[3]);
   //   var primitiveType = gl.TRIANGLES;
   //   var offset = 0;
   //   var count = 6;
